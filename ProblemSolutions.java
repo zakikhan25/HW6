db6e4de4ba75616b03ecf4bcf41069b83b2de626
zakikhan25 <zkhan25@luc.edu>
2025-04-03 22:53:53 -0500

    Update ProblemSolutions.java

--- ProblemSolutions.java
+++ ProblemSolutions.java
@@ -1,74 +1,150 @@
 
 /******************************************************************
  *
- *   Zaki Khan / 272 001
+ * Zaki Khan / 272 001
  *
- *   Correct Problem Solutions Implementation
+ * This java file contains the problem solutions for the methods lastBoulder,
+ * showDuplicates, and pair methods. You should utilize the Java Collection
+ * Framework for these methods.
  *
  ********************************************************************/
-
 import java.util.*;
+import java.util.PriorityQueue;
 
 public class ProblemSolutions {
+
+    /**
+     * Priority Queue (PQ) Game
+     *
+     * PQ1 Problem Statement:
+     * -----------------------
+     *
+     * You are given an array of integers of boulders where boulders[i] is the
+     * weight of the ith boulder.
+     *
+     * We are playing a game with the boulders. On each turn, we choose the heaviest
+     * two boulders and smash them together. Suppose the heaviest two boulders have
+     * weights x and y. The result of this smash is:
+     *
+     * If x == y, both boulders are destroyed, and
+     * If x != y, the boulder of weight x is destroyed, and the boulder of
+     * weight y has new weight y - x.
+     *
+     * At the end of the game, there is at most one boulder left.
+     *
+     * Return the weight of the last remaining boulder. If there are no boulders
+     * left, return 0.
+     *
+     * Example 1:
+     *
+     * Input: boulders = [2,7,4,1,8,1]
+     * Output: 1
+     * Explanation:
+     * We combine 7 and 8 to get 1 so the list converts to [2,4,1,1,1] then,
+     * we combine 2 and 4 to get 2 so the list converts to [2,1,1,1] then,
+     * we combine 2 and 1 to get 1 so the list converts to [1,1,1] then,
+     * we combine 1 and 1 to get 0 so the list converts to [1] then that's the
+     * value of the last stone.
+     *
+     * Example 2:
+     *
+     * Input: boulders = [1]
+     * Output: 1
+     *
+     * RECOMMENDED APPROACH
+     *
+     * Initializing Priority Queue in reverse order, so that it gives
+     * max element at the top. Taking top Elements and performing the
+     * given operations in the question as long as 2 or more boulders;
+     * returning the 0 if queue is empty else return pq.peek().
+     */
     public static int lastBoulder(int[] boulders) {
-        PriorityQueue<Integer, Integer> maxHeap = new PriorityQueue<>(
-            boulders.length,
-            Collections.reverseOrder()
-        );
-        
-        for (int stone : boulders) {
-            maxHeap.add(stone, stone);
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int boulder : boulders) {
+            pq.add(boulder);
         }
-        
-        while (maxHeap.size() > 1) {
-            int y = maxHeap.poll().value();
-            int x = maxHeap.poll().value();
-            if (x != y) {
-                maxHeap.add(y - x, y - x);
+        while (pq.size() > 1) {
+            int stone1 = pq.poll();
+            int stone2 = pq.poll();
+            if (stone1 != stone2) {
+                pq.add(stone1 - stone2);
             }
         }
-        
-        return maxHeap.isEmpty() ? 0 : maxHeap.poll().value();
+        return pq.isEmpty() ? 0 : pq.peek();
     }
 
+    /**
+     * Method showDuplicates
+     *
+     * This method identifies duplicate strings in an array list. The list
+     * is passed as an ArrayList<String> and the method returns an ArrayList<String>
+     * containing only unique strings that appear more than once in the input list.
+     * This returned array list is returned in sorted ascending order. Note that
+     * this method should consider case (strings are case-sensitive).
+     *
+     * For example, if the input list was: "Lion", "Dog", "Cat", "Dog", "Horse", "Lion", "CAT"
+     * the method would return an ArrayList<String> containing: "Dog", "Lion"
+     *
+     * @param input an ArrayList<String>
+     * @return an ArrayList<String> containing only unique strings that appear
+     * more than once in the input list. They will be in ascending order.
+     */
     public static ArrayList<String> showDuplicates(ArrayList<String> input) {
-        Map<String, Integer> freq = new HashMap<>();
-        ArrayList<String> result = new ArrayList<>();
-        
-        for (String s : input) {
-            freq.put(s, freq.getOrDefault(s, 0) + 1);
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String str : input) {
+            map.put(str, map.getOrDefault(str, 0) + 1);
         }
-        
-        for (Map.Entry<String, Integer> e : freq.entrySet()) {
-            if (e.getValue() > 1) {
-                result.add(e.getKey());
+        ArrayList<String> result = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > 1) {
+                result.add(entry.getKey());
             }
         }
-        
         Collections.sort(result);
         return result;
     }
 
+    /**
+     * Finds pairs in the input array that add up to k.
+     *
+     * @param input Array of integers
+     * @param k The sum to find pairs for
+     * @return an ArrayList<String> containing a list of strings. The ArrayList
+     * of strings needs to be ordered both within a pair, and
+     * between pairs in ascending order. E.g.,
+     *
+     * - Ordering within a pair:
+     * A string is a pair in the format "(a, b)", where a and b are
+     * ordered lowest to highest, e.g., if a pair was the numbers
+     * 6 and 3, then the string would be "(3, 6)", and NOT "(6, 3)".
+     * - Ordering between pairs:
+     * The ordering of strings of pairs should be sorted in lowest to
+     * highest pairs. E.g., if the following two string pairs within
+     * the returned ArraryList, "(3, 6)" and "(2, 7), they should be
+     * ordered in the ArrayList returned as "(2, 7)" and "(3, 6 )".
+     *
+     * Example output:
+     * If the input array list was {2, 3, 3, 4, 5, 6, 7}, then the
+     * returned ArrayList<String> would be {"(2, 7)", "(3, 6)", "(4, 5)"}
+     *
+     * HINT: Considering using any Java Collection Framework ADT that we have used
+     * to date, though HashSet. Consider using Java's "Collections.sort()" for final
+     * sort of ArrayList before returning so consistent answer. Utilize Oracle's
+     * Java Framework documentation in its use.
+     */
     public static ArrayList<String> pair(int[] input, int k) {
-        Arrays.sort(input);
         ArrayList<String> result = new ArrayList<>();
-        int left = 0, right = input.length - 1;
-        
-        while (left < right) {
-            int sum = input[left] + input[right];
-            if (sum == k) {
-                result.add("(" + input[left] + ", " + input[right] + ")");
-                while (left < right && input[left] == input[left+1]) left++;
-                while (left < right && input[right] == input[right-1]) right--;
-                left++;
-                right--;
-            } else if (sum < k) {
-                left++;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : input) {
+            int complement = k - num;
+            if (map.getOrDefault(complement, 0) > 0) {
+                result.add("(" + Math.min(num, complement) + ", " + Math.max(num, complement) + ")");
+                map.put(complement, map.get(complement) - 1);
             } else {
-                right--;
+                map.put(num, map.getOrDefault(num, 0) + 1);
             }
         }
-        
+        Collections.sort(result);
         return result;
     }
 }
